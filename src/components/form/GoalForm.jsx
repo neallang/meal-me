0e75a45ef9aacb342e2a75f8868bc3801d19c626
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-12 15:41:10 -0400

    part 3 structure

--- src/components/form/GoalForm.jsx
+++ src/components/form/GoalForm.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
 import { saveFormData } from "../../firebase/auth";
+import './goal-form.css'
 
 const GoalForm = ({ formData, setFormData, userID }) => {
     const navigate = useNavigate();
@@ -21,64 +22,90 @@ const GoalForm = ({ formData, setFormData, userID }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form id="goal-form" onSubmit={handleSubmit}>
             <h1>What Are Your Goals?</h1>
-            <div>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="goal" 
-                    value="weightLoss" 
-                    checked={formData.goal === 'weightLoss'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Weight Loss
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="goal" 
-                    value="mildWeightLoss" 
-                    checked={formData.goal === 'mildWeightLoss'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Mild Weight Loss
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="goal" 
-                    value="maintainWeight" 
-                    checked={formData.goal === 'maintainWeight'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Maintain Weight
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="goal" 
-                    value="mildWeightGain" 
-                    checked={formData.goal === 'mildWeightGain'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Mild Weight Gain
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="goal" 
-                    value="weightGain" 
-                    checked={formData.goal === 'weightGain'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Weight Gain
-                </label>
+            <p>Where do you want to be in 6 months? What are your long-term health goals?</p>
+            <div className="form-content">
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="goal" 
+                        value="weightLoss" 
+                        checked={formData.goal === 'weightLoss'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Weight Loss
+                    </label>
+                    <div className="description">
+                        <p>~1 lb/week</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="goal" 
+                        value="mildWeightLoss" 
+                        checked={formData.goal === 'mildWeightLoss'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Mild Weight Loss
+                    </label>
+                    <div className="description">
+                        <p>~0.5 lb/week</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="goal" 
+                        value="maintainWeight" 
+                        checked={formData.goal === 'maintainWeight'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Maintain Weight
+                    </label>
+                    <div className="description">
+                        <p>0 lb/week</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="goal" 
+                        value="mildWeightGain" 
+                        checked={formData.goal === 'mildWeightGain'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Mild Weight Gain
+                    </label>
+                    <div className="description">
+                        <p>~0.5 lb/week</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="goal" 
+                        value="weightGain" 
+                        checked={formData.goal === 'weightGain'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Weight Gain
+                    </label>
+                    <div className="description">
+                        <p>~1 lb/week</p>
+                    </div>
+                </div>
             </div>
             <button type="submit">Submit</button>
         </form>

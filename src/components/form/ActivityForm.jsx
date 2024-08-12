8cbabd97fcc85e8cc2e243ee8e24bdad4d421db0
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-12 15:30:40 -0400

    form part2 structured

--- src/components/form/ActivityForm.jsx
+++ src/components/form/ActivityForm.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
+import './activity-form.css'
 
 const ActivityForm = ({ formData, setFormData }) => {
     const navigate = useNavigate();
@@ -19,64 +20,95 @@ const ActivityForm = ({ formData, setFormData }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form id="activity-form" onSubmit={handleSubmit}>
             <h1>Describe Your Activity Level</h1>
-            <div>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="activity" 
-                    value="inactive" 
-                    checked={formData.activity === 'inactive'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Inactive
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="activity" 
-                    value="mildlyActive" 
-                    checked={formData.activity === 'mildlyActive'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Mildly Active
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="activity" 
-                    value="active" 
-                    checked={formData.activity === 'active'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Active
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="activity" 
-                    value="veryActive" 
-                    checked={formData.activity === 'veryActive'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Very Active
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    name="activity" 
-                    value="extremelyActive" 
-                    checked={formData.activity === 'extremelyActive'} 
-                    onChange={handleChange} 
-                    required 
-                    />
-                    Extremely Active
-                </label>
+            <p>Select the most applicable choice to your activity level. Just be honest - we don't judge!</p>
+            <div className="form-content">
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="activity" 
+                        value="inactive" 
+                        checked={formData.activity === 'inactive'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Inactive
+                    </label>
+                    <div className="description">
+                        <p>Little to no exercise</p>
+                        <p>(0-1 times per week)</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="activity" 
+                        value="mildlyActive" 
+                        checked={formData.activity === 'mildlyActive'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Mildly Active
+                    </label>
+                    <div className="description">
+                        <p>Light to moderate exercise</p>
+                        <p>(2-3 times per week)</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="activity" 
+                        value="active" 
+                        checked={formData.activity === 'active'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Active
+                    </label>
+                    <div className="description">
+                        <p>Moderate to intense exercise</p>
+                        <p>(4-5 times per week)</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="activity" 
+                        value="veryActive" 
+                        checked={formData.activity === 'veryActive'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Very Active
+                    </label>
+                    <div className="description">
+                        <p>Moderate to intense exercise</p>
+                        <p>(6-7 times per week)</p>
+                    </div>
+                </div>
+                <div className="form-el">
+                    <label>
+                        <input 
+                        type="radio" 
+                        name="activity" 
+                        value="extremelyActive" 
+                        checked={formData.activity === 'extremelyActive'} 
+                        onChange={handleChange} 
+                        required 
+                        />
+                        Extremely Active
+                    </label>
+                    <div className="description">
+                        <p>Intense exercise daily or physical job</p>
+                        <p>(7 times per week)</p>
+                    </div>
+                </div>
             </div>
             <button type="submit">Next</button>
         </form>

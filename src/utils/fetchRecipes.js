04bbbc217da6311ff1e848e70c87d191e80c5c9c
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-13 12:28:08 -0400

    WORKING API CALL!

--- src/utils/fetchRecipes.js
+++ src/utils/fetchRecipes.js
@@ -1,22 +1,34 @@
 import axios from 'axios';
 
 export const getRecipes = async (caloriesPerDay) => {
-  const appId = '4ad0c8b8';
-  const appKey = 'deb0c52cea35b8da7dfa5b4243c6a73d';
-  const calories = await caloriesPerDay.toString();
-  console.log(calories);
+  const appId = '20c6cf6d';
+  const appKey = 'de90d131818e63d03aaaa4770b73a8b1';
+  // const calories = await caloriesPerDay.toString();
+  const calories = '700-900';
   try {
     const response = await axios.get('https://api.edamam.com/api/recipes/v2', {
       params: {
+        type: 'public',
         app_id: appId,
         app_key: appKey,
-        calories: `lte ${caloriesPerDay}`,
-        to: 1,
+        calories: calories,
+        to: 20, // Retrieve up to 20 results
       },
     });
 
     if (response.data.hits && response.data.hits.length > 0) {
-      return response.data.hits[0].recipe;
+      // Filter the results based on calories
+      const filteredRecipes = response.data.hits.filter(hit => {
+        const recipeCalories = hit.recipe.calories;
+        return recipeCalories >= 200 && recipeCalories <= 1100;
+      });
+
+      if (filteredRecipes.length > 0) {
+        return filteredRecipes[0].recipe;
+      } else {
+        console.log('No recipes found within the calorie range.');
+        return null;
+      }
     } else {
       console.log('No recipes found for the given calorie count.');
       return null;

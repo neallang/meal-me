8dfe321dd5ee93dc2c570e3bcab1f878c880b8ad
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-15 11:46:14 -0400

    styling

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -3,14 +3,25 @@ import './recipe.css'
 const Recipe = ({recipe, currentMeal}) => {
 
     console.log(recipe)
+    console.log(recipe.calories / 4)
+    console.log(recipe.totalDaily.PROCNT.quantity / recipe.yield)
+    console.log(recipe.totalDaily.CHOCDF.quantity / recipe.yield)
+    console.log(recipe.totalDaily.FAT.quantity / recipe.yield)
 
 
     return (
         <div id="recipe">
-            <h1>{currentMeal}</h1>
-            <h2>{recipe.label}</h2>
-            <img src={recipe.image}/>
-            <a href={recipe.url} target='_blank'>recipe</a>
+            <h1>{recipe.label}</h1>
+            <div className='recipe-details'>
+                <div className='nutrients'>
+                    <p>Calories: </p>
+                    <p>Protein: </p>
+                    <p>Carbohydrates: </p>
+                    <p>Fat: </p>
+                </div>
+                <img src={recipe.image}/>
+            </div>
+            <a href={recipe.url} target='_blank'>View Full Recipe</a>
         </div>
     )
 

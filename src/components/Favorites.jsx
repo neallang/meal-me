17ecced10b2c42d00bf389600b410150f75318f0
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-21 12:20:20 -0400

    navigate home functionality when no favorites

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Recipe from './Recipe';
 import { setFavoritesToLocalStorage, setFavoritesInfoToLocalStorage, getFavoritesFromLocalStorage, getFavoritesInfoFromLocalStorage } from '../utils/favorites';
 import { useAuth } from "../contexts/authContext";
+import { useNavigate } from 'react-router-dom';
+import './favorites.css';
 
 const Favorites = () => {
     const { currentUser } = useAuth();
     const [userID, setUserID] = useState();
     const [favorites, setFavorites] = useState([]);
     const [favoritesInfo, setFavoritesInfo] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (currentUser) {
@@ -21,9 +24,18 @@ const Favorites = () => {
         }
     }, [currentUser]);
 
+    const handleGoHome = () => {
+        navigate('/home');
+      };
+
     if (favorites.length === 0 || favoritesInfo.length === 0) {
-        return <p>No favorite recipes found.</p>;
-    }
+        return (
+        <div style={{ textAlign:'center'}}>
+            <h2>No favorite recipes found.</h2>
+            <button onClick={handleGoHome}>Back to Home</button>
+        </div>
+        );
+    };
 
     const toggleFavorite = (recipe) => {
         setFavorites((prevFavorites) => {

3b580fa42fd6485c288522b1bd6ff0cb6f66cc40
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-23 11:38:03 -0400

    favicon

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,8 +13,8 @@ const Landing = () => {
 
     return (
         <div id="landing">
-            <h1>Meal Me</h1>
-            <p>Welcome to Meal Me, a nutrition website that reccommends recipes for you based on your health goals! Sign in below to get started!</p>
+            <h1>Meal Me!</h1>
+            <p>Welcome to Meal Me! - a nutrition website that reccommends recipes for you based on your health goals! Sign in below to get started!</p>
             {signUpPage ? <SignUp toggleAuthMode={toggleAuthMode} /> : <SignIn toggleAuthMode={toggleAuthMode} />}
             
         </div>

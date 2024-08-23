386d3588d567917edb0c5b560d65c4f6e46b65fb
Neal Langhorne <neal_langhorne@yahoo.com>
2024-08-23 11:26:56 -0400

    done

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,7 +14,7 @@ const Landing = () => {
     return (
         <div id="landing">
             <h1>Meal Me</h1>
-            <p>Welcome to Meal Me, an AI-powered website that reccommends recipes for you based on your health goals! Sign in below to get started!</p>
+            <p>Welcome to Meal Me, a nutrition website that reccommends recipes for you based on your health goals! Sign in below to get started!</p>
             {signUpPage ? <SignUp toggleAuthMode={toggleAuthMode} /> : <SignIn toggleAuthMode={toggleAuthMode} />}
             
         </div>
